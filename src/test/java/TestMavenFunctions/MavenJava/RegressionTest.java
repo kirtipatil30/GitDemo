2793583f76e19cffebdc9d4e367f26ba99f50fe7
kirti <kirtiidpatil@gmail.com>
2021-06-13 16:10:22 +0530

    changes added by kirt

--- src/test/java/TestMavenFunctions/MavenJava/RegressionTest.java
+++ src/test/java/TestMavenFunctions/MavenJava/RegressionTest.java
@@ -6,6 +6,11 @@ public class RegressionTest {
 	@Test
 	public void f1() {
 		System.out.println("Inside f1() of Regression");
+	} 
+	
+	@Test
+	public void f2() {
+		System.out.println("New test added to regresion");
 	}
 
 }
