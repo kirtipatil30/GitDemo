ffc4cccea3d664b2c47e50287e0f01b09bc11833
kirti <kirtiidpatil@gmail.com>
2021-06-13 17:26:35 +0530

    changed files by gitstuff

--- src/test/java/TestMavenFunctions/MavenJava/RegressionTest.java
+++ src/test/java/TestMavenFunctions/MavenJava/RegressionTest.java
@@ -6,11 +6,18 @@ public class RegressionTest {
 	@Test
 	public void f1() {
 		System.out.println("Inside f1() of Regression");
+		System.out.println("New test added to 1 Git stuff made changes to f1");
+		System.out.println("New test added to 1 Git stuff made changes to f1");
+		System.out.println("New test added to 1 Git stuff made changes to f1");
+
 	} 
 	
 	@Test
 	public void f2() {
 		System.out.println("New test added to regresion");
+		System.out.println("New test added to 1 GitStuff  made changes  to f2");
+		System.out.println("New test added to 2 Git Stuff  made changes to f2");
+		System.out.println("New test added to 3 Git stuff made changes to f2");
 	}
 
 }
