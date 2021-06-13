e58e2bfa28bb576b6a91141e5045cf17197e68c1
kirti <kirtiidpatil@gmail.com>
2021-06-13 19:58:56 +0530

    added by SD

---
+++ src/test/java/TestMavenFunctions/MavenJava/NewClassforGitDemo.java
@@ -0,0 +1,16 @@
+package TestMavenFunctions.MavenJava;
+
+public abstract class NewClassforGitDemo {
+	
+	 public abstract void add();
+	 
+	 public abstract void edit();
+	 
+	 public abstract void delete();
+	 
+	 public void Read() {
+		 System.out.println("Read class file NewClassforGitDemo ");
+	 }
+
+
+}
